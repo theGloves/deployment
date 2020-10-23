10f0d2d2b98ad96c8f9b4c9d84428da946c20c37
theGloves <yusfko@hotmail.com>
2020-10-23 04:35:05 +0000

    修改测试工具

--- Makefile
+++ Makefile
@@ -9,14 +9,14 @@ install:
 # 快速清除部署的应用
 clean:
 	# 根据label删除deployment&service
-	./bin/kubectl delete services,deployments -l app=shardingbc
+	./bin/kubectl delete -n tendermint services,deployments -l app=shardingbc
 	curl -XPOST "http://127.0.0.1:9200/tendermint-20201007/_delete_by_query" -H 'Content-Type: application/json' -d'{  "query": {    "terms": {      "@log_name": ["tendermint.error","tendermint.tps","tendermint.latency"]    }  }}'
 
 # 快速部署应用
 quick:
 	# 部署k8s服务&deployment
 	bash sh/sendmasterfile.sh >/dev/null 2>&1
-	./bin/kubectl create -f network/docker-compose.yaml 
+	./bin/kubectl create -n tendermint -f network/docker-compose.yaml 
 
 test:
 	bash batch_test.sh -n 4,8,16 -r 99999 -s 2 -t 200,400,600,800,1000 -i 60 -d 120
--- py/simulator.py
+++ py/simulator.py
@@ -11,7 +11,7 @@ rate = sys.argv[5]  # rate
 shard="0"
 if shard_count == 1:
 
-    cmd = "cd dist;./tm-bench_simulator -rate " + rate + " -r " + r + " -T " + T +" -shard "+shard+ " 0S1.tendermint:26657"
+    cmd = "cd dist;./tm-bench_simulator -rate " + rate + " -r " + r + " -T " + T +" -shard "+shard+ " tt0s1:26657"
     print(cmd)
 else:
     Send_shard = ""  # 要发往哪些分片
@@ -26,8 +26,7 @@ else:
     flag = 0
     for i in range(0, shard_count ):
         shard_name = str(i)
-        # shard_node = shard_name + "S1.tendermint:26657"
-        shard_node = shard_name+"S"+str(random.randint(1,int(send_count)))+".tendermint:26657"
+        shard_node = "tt"+shard_name+"s"+str(random.randint(1,int(send_count)))+":26657"
         cmd = "cd dist;./tm-bench_simulator -rate " + rate + " -r " + r + " -T " + T + " -shard " +shard_name+" -as " + Send_shard + " " + shard_node
         if i == (shard_count-1):
             cmd = "./tm-bench_simulator -rate " + rate + " -r " + r + " -T " + T + " -shard " +shard_name+" -as " + Send_shard + " " + shard_node
